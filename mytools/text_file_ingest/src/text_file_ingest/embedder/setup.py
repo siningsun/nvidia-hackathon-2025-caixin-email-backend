f48c0c144c5d48d36c69e4812c24e8f190d9fb7f
siningsun <690058381@qq.com>
2025-09-04 18:51:21 +0800

    Update PID files, modify text file ingestion descriptions, and remove obsolete files; add US Open 2025 data

--- mytools/text_file_ingest/src/text_file_ingest/embedder/setup.py
+++
@@ -1,8 +0,0 @@
-# setup.py
-from setuptools import setup, find_packages
-
-setup(
-    name="embedder",
-    version="0.1",
-    packages=find_packages(),
-)
--- test.py
+++
@@ -1,4 +0,0 @@
-# check_builder.py
-from nat.builder import builder
-
-print(dir(builder))
